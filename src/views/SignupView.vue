b5e8acc3f587f612ef690976e5f4b1e5b0e7dac1
Jason <147480420+JsonSept@users.noreply.github.com>
2024-03-06 16:40:03 +0200

    Add files via upload

---
+++ src/views/SignupView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>This is the signup page</h1>
+  </div>
+  <div>
+    <input type="text" name="username" placeholder="username" v-model="username"><br><br>
+    <input type="text" name="password" placeholder="password" v-model="password"><br><br>
+    <button @click='registerUser()'>SignUp</button>
+  </div>
+
+<!-- <router-link to="user"><button>Signup</button></router-link> -->
+</template>
+
+
+<script>
+export default {
+  data(){
+    return {
+      username:null,
+      password:null
+    }
+  },
+  computed: {
+    registerUser(){
+      this.$Store.dispatch("registerUser",this.$data)
+    }
+  }
+}
+
+</script>
