ce881b93b68a8ba1512ba32107ee73f6c20156b6
Jason <147480420+JsonSept@users.noreply.github.com>
2024-03-06 16:33:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,81 @@
-import {getUser,getUsers,deleteUser,addUser} from '../models/database.js'
+import express from 'express'
+import cors from 'cors'
+import {config} from 'dotenv'
+// import productsRouter from './routes/products.js'
+// import usersRouter from './routes/users.js'
+import bcrypt from 'bcrypt'
+import cookieParser from 'cookie-parser'
+import jwt from 'jsonwebtoken'
+import{getUser,getUsers,deleteUser,addUser,checkUser} from './models/database.js'
+config();
 
-export default {
-    getOne: async(req,res)=>{
-        res.send(await getUser(+req.params.id))
-    },
-    getMore: async(req,res)=>{
-        res.send(await getUsers())
-    },
-    getPost : async(req,res)=>{
-        const {username,password} = req.body
-        bcrypt.hash(password,10,async(err)=>{
-            if (err) throw err
-            const post = await addUser(username,password)
-            res.send(await getUsers())
+const PORT = process.env.PORT
+const app = express()
 
-            const token = jwt.sign({ username, SECRET_KEY }, { expiresIn: '1h' });
-        res.cookie('token', token, {httpOnly: true});
-        res.send({
-            msg: "Your account has been created successfully"
-        })
-    });
-    },
-    getDelete : async(req,res)=>{ 
-        res.send(await deleteUser(+req.params.name))
-        alert('user has been removed from the database')
-        },
-    // getPatch : async(req,res)=>{ 
-    //     const [friend] = await getUser(+req.params.id)
-    //     let {name,age} = req.body
-    //     name? name=name: {name}= friend
-    //     email? email=email: {email}= friend
-    //     age? age=age: {age}=friend
-      
-    //     await editUser(name,email,age)
-    //     res.json(await getUsers())
-    //     }
+app.use(cors())
+app.use(express.json())
+
+app.use(cookieParser())
+
+app.get('/users',(req,res)=>{
+    res.json(users)
+});
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params;
+    res.json(users);
+  });
+
+  app.use('/users',usersRouter)
+
+const authenticate = (req,res,next) =>{
+    let {cookie} = req.headers
+    let tokenInHeader =cookie && cookie.split('=')[1]
+    if(tokenInHeader===null) res.sendStatus(401)
+    jwt.verify(tokenInHeader,process.env.SECRET_KEY,(err,user)=>{
+if(err) return res.sendStatus(403)
+req.user = user
+next()
+})
 }
+
+
+
+
+app.post('/users',(req,res)=>{
+    const {username,password} =req.body
+    bcrypt.hash(password,10, async (err,hash)=>{
+        if(err) throw err
+        await addUser(username,hash)
+    })
+    res.send({
+        msg: "You have registered successfully"
+    })
+})
+
+const auth = async (req,res,next) => {
+    const {password,username} = req.body
+    const hashedPassword = await chechUser(username)
+    bcrypt.compare(password,hashedPassword, (err,result)=>{
+        if(err) throw err
+        if(result === true){
+            const {username} = req.body
+    const token = jwt.sign({username:username},
+    process.env.SECRET_KEY,{expiresIn:'1h'})
+    res.cookie('jwt',token)
+            next()
+        }else{
+            res.send({msg:'The username or password is incorrect'})
+        }
+    })
+}
+
+app.post('/login',auth, (req,res)=>{
+    res.send({
+        msg:'You have logged in!!! YAY!'
+    })
+})
+
+app.listen(PORT, ()=>{
+    console.log('http://localhost:'+ PORT);
+})
